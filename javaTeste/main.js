116eb85435c8dcbb04e0cba6b88c06c5c33b8acb
sandroCorrrea <jsandro800@gmail.com>
2021-01-10 13:36:02 -0300

    encho3x

---
+++ javaTeste/main.js
@@ -0,0 +1,9 @@
+function porcima(elemento)
+{
+	elemento.innerHTML = "Voce passou o mouse aqui";
+}
+
+function tirandoOmouse(elemento)
+{
+	elemento.innerHTML = "<h1>Voce tirou o mouse</h1>";
+}
---
+++ jsAvançado/main.js
@@ -0,0 +1,42 @@
+/*var somaOld = function(a, b)
+{
+	return a + b;
+}
+
+soma = console.log(somaOld(5, 8));*/
+
+/*var soma = (a, b) => (a +b);
+
+console.log(soma(18, 36));*/
+
+/*var validao = (a, b) => {
+	var x = 10;
+
+	if (x == 10){
+		console.log("Verdade");
+	}
+}
+
+console.log(validao(1, 5));
+*/
+
+/*var objeto = () => ({ test: 123});
+
+console.log(objeto());*/
+
+
+/*var obj = {
+	showContext: function showContext()
+	{
+		setTimeout(() => {
+			this.log("after 2000ms");
+		}, 3000);
+	}, 
+	log: function log(value){
+		console.log(value);
+	}
+}
+obj.showContext()*/
+
+
+
