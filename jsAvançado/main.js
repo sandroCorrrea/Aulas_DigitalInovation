67a4c9864e3f8d2d753c3157670b1769a93500fb
sandroCorrrea <jsandro800@gmail.com>
2021-01-13 20:29:00 -0300

    EmpurrandoJsAvancado

--- jsAvançado/main.js
+++ jsAvançado/main.js
@@ -212,7 +212,7 @@ for (let values of obj) {
 }*/
 
 //GENERATORS
-function* hello() {
+/*function* hello() {
 	console.log("Hello");
 	yield;
 
@@ -223,4 +223,85 @@ function* hello() {
 
 const gene = hello();
 
-console.log(gene.next()); console.log(gene.next()); console.log(gene.next()); console.log(gene.next());
+console.log(gene.next()); console.log(gene.next()); console.log(gene.next()); console.log(gene.next());*/
+
+//USANDO CALLBACKS
+/*
+function doSomething(callback) {
+	setTimeout(function () {
+		callback("Name: Sandro Correa");
+	}, 1000);
+};
+
+function doSomthing2(callback) {
+	setTimeout(function () {
+		callback("Surname: Rocha Junior");
+	}, 1000);
+};
+
+function doAll() {
+	try {
+
+		doSomething(function (name) {
+			var response = name.split(" ");
+			try {
+
+				doSomthing2(function (name2) {
+					var response2 = name2.split(" ");
+					try {
+
+						setTimeout(function (display) {
+							console.log(response, response2);
+						});
+					} catch (err) {
+
+                    }
+				});
+			} catch (err) {
+
+            }
+		});
+	} catch (err) {
+
+    }
+};
+
+doAll();
+*/
+
+//PROMISSES
+
+/*const doSomething = new Promise((resolve, reject) => {
+	//throw new Error("Erro muito cabuloso");
+	setTimeout(function () {
+		resolve("Neme: Sandro Junior");
+	}, 1000);
+});
+
+const doSomething2 = new Promise((resolve, reject) => {
+	setTimeout(function () {
+		resolve("Correa Rocha")
+	}, 1000);
+});
+
+//ENCHUTANDO ERROS
+//doSomething()
+//	.then(date => {
+//		console.log(date);
+//		return doSomething2;
+//	})
+//	.then(date => console.log(date))
+//	.cath(error => console.log('ops', error));
+
+Promise.all([doSomething, doSomething2]).then(date => {
+	console.log(date[0].split(""));
+	console.log(date[1].split(""));
+});
+*/
+
+fetch('/data.json').then(responseStream  => {
+	return responseStream.json().then(data => {
+		console.log(data);
+	});
+});
+
