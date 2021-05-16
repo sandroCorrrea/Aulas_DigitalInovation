6b60b73927a050e6e0b046e4d02f612ef39df097
sandroCorrrea <jsandro800@gmail.com>
2021-05-16 15:10:17 -0300

    Tipos de Elemeneto js

--- jsAvançado/testeJs(01)/testeJs(01)/server.js
+++ jsAvançado/testeJs(01)/testeJs(01)/server.js
@@ -1,10 +1,68 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+// //alert("Ola usuario");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+// // document.getElementById("nome").value = "oi";
+
+// // var texto = "Lorem Ipsum é simplesmente uma simulação de texto, ";
+
+// // var nome = parseInt(prompt("Digite um valor")) ;
+// // var nome2 = parseInt(prompt("Digite um valor")) ;
+
+// // // nome  = parseInt(nome);
+// // // nome2 = parseInt(nome2);
+
+// // alert("A soma foi de : " + (nome + nome2))
+
+// // document.write(texto);
+
+// // document.write("<h1>" + nome + "</h1>");
+
+// // document.getElementById("nome").value = "Sandro Junior";
+
+// // function areaTerreno(largura, latitude)
+// // {
+// //     var area = largura * latitude;
+
+// //     return area;
+// // }
+
+// // largura  =  prompt("Digite a Largura do Terreno");
+// // latitude =  prompt("Digite a Latitude do Terreno");
+
+// // alert(areaTerreno(largura, latitude));
 
 
+// function callback(id, callbackTrue, callbackFase)
+// {
+//     if(false)
+//     {
+//         callbackTrue("Menssagem criada com sucesso", "Menssagem Curta");
+//     }
+//     else{
+//         callbackFase("Messagem de Erro");
+//     }
+// }
+
+// var callbackTrue = function(Menssagem, tipo)
+// {
+//     document.write("Essa é a primeira menssagem: " + Menssagem);
+//     document.write("<br/>");
+//     document.write("Essa ja é a segunda: " + tipo);
+// }
+
+// var callbackFase = function(erro)
+// {
+//     document.write("Menssagem de erro : " + erro);    
+// }
+
+// callback(1, callbackTrue, callbackFase);
+
+
+// var data = new Date;
+
+// document.write(data);
+
+
+function disparaAlerts()
+{
+    alert("Voce clicou na div");
+}
